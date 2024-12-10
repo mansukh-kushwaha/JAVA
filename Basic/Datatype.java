3dcf0da2255e4cd0b7f266b944d04b6b3e6e0d5c
Mansukh <mansukhk20@outlook.com>
2024-12-10 20:52:39 +0530

    All Datatypes

---
+++ Basic/Datatype.java
@@ -0,0 +1,35 @@
+public class Datatype {
+    public static void main(String[] args) {
+        // byte
+        byte num = 34;
+        System.out.println("byte " + num);
+
+        // short 
+        short num1 = 12345;
+        System.out.println("short " + num1);
+
+        // int
+        int num2 = 475;
+        System.out.println("the value is " + num2);
+
+        // long
+        long num3 = 2348l;
+        System.out.println("The value is "+ num3);
+
+        // float
+        float num4=675.3f;
+        System.out.println("float :"+num4);
+
+        // Double
+        double num5=23.4;
+        System.out.print("Double"+num5);
+
+        // char 
+        char a = 'o';
+        System.out.println("character : " + a);
+
+        // boolean 
+        boolean b = true;
+        System.out.println("boolean :" + b);
+    }
+}
