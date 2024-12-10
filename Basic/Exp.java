41d1bd08040b0d578a83f0f33a9f962086be6639
Mansukh <mansukhk20@outlook.com>
2024-12-10 21:38:36 +0530

    Sum of 5 number

---
+++ Basic/Exp.java
@@ -0,0 +1,11 @@
+public class Exp {
+    public static void main(String[] args){
+        int a=34;
+        int b=67;
+        int c=23;
+        int d=12;
+        int e=6;
+        int f=a+b+c+d+e;
+        System.out.println(f);
+    }
+}
