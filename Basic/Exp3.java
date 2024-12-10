4fe83b806f7437936893d1200650a5ffcbe8451c
Mansukh <mansukhk20@outlook.com>
2024-12-10 21:43:56 +0530

    Operator

---
+++ Basic/Exp3.java
@@ -0,0 +1,11 @@
+public class Exp3 {
+    public static void main(String[] args) {
+        int a=56;
+        int b=34;
+        int c=12;
+        int d=2;
+        int e=13;
+        int f=a*b+c/d-e;
+        System.out.println(f);
+    }
+}
