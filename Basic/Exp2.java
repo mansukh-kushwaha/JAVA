d435f6f2d10c59ab4944822683ec8dad3393c756
Mansukh <mansukhk20@outlook.com>
2024-12-10 21:40:58 +0530

    Sum of 4 number

---
+++ Basic/Exp2.java
@@ -0,0 +1,10 @@
+public class Exp2 {
+    public static void main(String[] args){
+        int a=23;
+        int b=89;
+        int c=12;
+        int d=56;
+        int e=a+b+c+d;
+        System.out.println(e);
+    }
+}
