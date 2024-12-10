31522f60c9a3c106dd04b0030aad25ffc0ddf445
Mansukh <mansukhk20@outlook.com>
2024-12-10 20:51:29 +0530

    Print Five Food Name

---
+++ Basic/FiveFood.java
@@ -0,0 +1,5 @@
+public class FiveFood {
+        public static void main(String[] var0) {
+           System.out.println("1=Pav Bhaji\n2=CholeBhature\n3=Jalebi-Fafda\n4=DaalBaati\n5=Maggie");
+        }
+}
