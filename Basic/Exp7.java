cc781aae7abf90e2bffee6b1ff6fe337481a27b0
Mansukh <mansukhk20@outlook.com>
2024-12-13 20:07:51 +0530

    Basic operation

---
+++ Basic/Exp7.java
@@ -0,0 +1,12 @@
+public class Exp7 {
+    public static void main(String[] args) {
+        int a=8;
+        int b=1;
+        int c=4;
+        int d=2;
+        int e=1;
+        int f= 6;
+        int g=a*b+(c-d)/e+f;
+        System.out.println(g);
+    }
+}
