8ad825af52085dff2a47d9b797cf3be1569b7215
Mansukh <mansukhk20@outlook.com>
2024-12-13 20:07:36 +0530

    Basic operation

---
+++ Basic/Exp6.java
@@ -0,0 +1,12 @@
+public class Exp6 {
+    public static void main(String[] args) {
+        double a=4.5;
+        double b=1.2;
+        int c=82;
+        int d=2;
+        double e=5.6;
+        double f= 1.234;
+        double g=a*b-(c/d)-e+f;
+        System.out.println(g);
+    }
+}
